0fa4966ac0af60f05a7d749bdbb308b841a71ed8
GlauberBalsani <glauber_29@hotmail.com>
2024-03-03 12:07:24 -0300

    first block

---
+++ src/main/java/noobchain/Main.java
@@ -0,0 +1,12 @@
+package noobchain;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Block genesisBlock = new Block("Hi im the first block", "0");
+		System.out.println("Hash for block 1 : " + genesisBlock.hash);
+
+	}
+
+}
